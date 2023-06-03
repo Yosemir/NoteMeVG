d9560c503b10630e0a238b06732e53263070232a
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2023-06-03 11:37:45 -0500

    cursos

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -61,39 +61,39 @@ export class ListComponent implements OnInit {
     console.log(this.user.email)
     this.serviceService.getNotas(this.user.email).subscribe(rest => {
       // Obtener las notas y headers para cada tabla
-      this.notaTabla1 = rest.filter(item => item.Curso === 'TEC');
+      this.notaTabla1 = rest.filter(item => item.Curso === 'IPT');
       const firstElementTabla1: Record<string, any> = this.notaTabla1[0];
       this.headersTabla1 = Object.keys(firstElementTabla1);
 
-      this.notaTabla2 = rest.filter(item => item.Curso === 'LYF');
+      this.notaTabla2 = rest.filter(item => item.Curso === 'ESG');
       const firstElementTabla2: Record<string, any> = this.notaTabla2[0];
       this.headersTabla2 = Object.keys(firstElementTabla2);
 
-      this.notaTabla3 = rest.filter(item => item.Curso === 'OEI');
+      this.notaTabla3 = rest.filter(item => item.Curso === 'CUA');
       const firstElementTabla3: Record<string, any> = this.notaTabla3[0];
       this.headersTabla3 = Object.keys(firstElementTabla3);
 
-      this.notaTabla4 = rest.filter(item => item.Curso === 'AED');
+      this.notaTabla4 = rest.filter(item => item.Curso === 'FUI');
       const firstElementTabla4: Record<string, any> = this.notaTabla4[0];
       this.headersTabla4 = Object.keys(firstElementTabla4);
 
-      this.notaTabla5 = rest.filter(item => item.Curso === 'HEI');
+      this.notaTabla5 = rest.filter(item => item.Curso === 'POO');
       const firstElementTabla5: Record<string, any> = this.notaTabla5[0];
       this.headersTabla5 = Object.keys(firstElementTabla5);
 
-      this.notaTabla6 = rest.filter(item => item.Curso === 'POE');
+      this.notaTabla6 = rest.filter(item => item.Curso === 'OE2');
       const firstElementTabla6: Record<string, any> = this.notaTabla6[0];
       this.headersTabla6 = Object.keys(firstElementTabla6);
 
-      this.notaTabla7 = rest.filter(item => item.Curso === 'AEC');
+      this.notaTabla7 = rest.filter(item => item.Curso === 'SOV');
       const firstElementTabla7: Record<string, any> = this.notaTabla7[0];
       this.headersTabla7 = Object.keys(firstElementTabla7);
 
-      this.notaTabla8 = rest.filter(item => item.Curso === 'CFD');
+      this.notaTabla8 = rest.filter(item => item.Curso === 'BD1');
       const firstElementTabla8: Record<string, any> = this.notaTabla8[0];
       this.headersTabla8 = Object.keys(firstElementTabla8);
 
-      this.notaTabla9 = rest.filter(item => item.Curso === 'IB1');
+      this.notaTabla9 = rest.filter(item => item.Curso === 'IB2');
       const firstElementTabla9: Record<string, any> = this.notaTabla9[0];
       this.headersTabla9 = Object.keys(firstElementTabla9);
 
@@ -101,9 +101,9 @@ export class ListComponent implements OnInit {
       const firstElementTabla10: Record<string, any> = this.notaTabla10[0];
       this.headersTabla10 = Object.keys(firstElementTabla10);
 
-      this.notaTabla11 = rest.filter(item => item.Curso === 'BOLETA');
+      /*this.notaTabla11 = rest.filter(item => item.Curso === 'BOLETA');
       const firstElementTabla11: Record<string, any> = this.notaTabla11[0];
-      this.headersTabla11 = Object.keys(firstElementTabla11);
+      this.headersTabla11 = Object.keys(firstElementTabla11);*/
 
       console.log("Finalizo la consulta");
     });
